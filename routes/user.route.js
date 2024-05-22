4dade2b0891805b9432fe2648e26ce19cb72ae0c
Arturo Puentes <arturo.puentes.yu@gmail.com>
2024-05-22 16:38:34 +0200

    fix: buil errors

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,6 +4,5 @@ const userController = require('../controllers/user.controller.js');
 
 
 router.get('/getUserById', userController.getUserById);
-router.post('/addUser', userController.addUser);
 
 module.exports = router;
